441c51893b11fc0375772271330988c7b268b861
Luis Rodriguez Fernandez <luis.rodriguezfernandez@sonarsource.com>
2025-05-30 09:49:44 +0200

    except issue

--- Person.py
+++ Person.py
@@ -6,6 +6,11 @@ class Person:
     if narg == 1:
        print('@Usage: input_filename nelements nintervals')
        break
+    a = {}
+    try:
+        a[5]
+    except KeyError:
+        raise  # Noncompliant
     return self
 
 p1 = Person("John", 36)
