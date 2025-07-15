1086bd6a17af6a25d98c52881a0a857e6da1968c
Luis Rodriguez Fernandez <luis.rodriguezfernandez@sonarsource.com>
2025-07-15 11:38:43 +0200

    python:S6882

--- Person.py
+++ Person.py
@@ -29,6 +29,9 @@ class Person:
     pass
     return self
 
+def foo():
+    dt = datetime(year=2024, day=66, month=1, hour=16, minute=1) # ValueError: day is out of range for month
+
 p1 = Person("John", 36)
 
 print(p1.name)
